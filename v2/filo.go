6324000d475ec096b60b8d9bcdc33c4459eb1212
Filippo Campegiani <filippo.campegiani@posteitaliane.it>
2021-03-14 12:04:08 +0100

    update.

---
+++ v2/filo.go
@@ -0,0 +1,11 @@
+// Package filo bla bla bla
+package filopackage
+
+import "github.com/filippocam/filopackage/v2/calendar"
+
+// Filo returns "ciao"
+func Filo() string {
+	date := calendar.Date{}
+	date.SetDescription("ciaooo")
+	return "ciao"
+}
---
+++ v2/greeting/greeting.go
@@ -0,0 +1,43 @@
+// Package greeting : descrizione package greeting
+package greeting
+
+import (
+	"fmt"
+	quoteV3 "rsc.io/quote/v3"
+	"unicode/utf8"
+)
+
+// MyConstant fsdsfd
+const MyConstant = 5
+
+// Hello   daasd
+// 	es.
+// 		greeting.Hello()
+//			restituisce la stringa Hello!
+func HelloV2() {
+	test()
+	fmt.Println("Hello!")
+	fmt.Println("len(quoteV3.Hello()): ", utf8.RuneCountInString(quoteV3.HelloV3()))
+	fmt.Println("quoteV3: ", quoteV3.HelloV3())
+}
+
+//Prova dsfsfds
+func Prova() {
+	fmt.Println("Provaaaaaa!")
+}
+
+//Prova dsfsfds
+func Prova2() {
+	fmt.Println("Provaaaaaa22222!")
+}
+
+// Hi dfsdfs
+func Hi() {
+	test()
+	fmt.Println("Hi!")
+}
+
+// test dfsdfs
+func test() {
+	fmt.Println("test!")
+}
